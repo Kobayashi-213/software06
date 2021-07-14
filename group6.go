8fae803695db607cc641537313ddaa1c9753e863
EC2 Default User <ec2-user@ip-172-31-41-52.ap-northeast-1.compute.internal>
2021-07-14 03:02:31 +0000

    modify develop 1

--- group6.go
+++ group6.go
@@ -2,26 +2,24 @@ package main
 
 import (
 	"fmt"
-	"os"
-
+	//"os"
 )
 
 type Board struct {
 	tokens []int // tokens[0] -> (0,0), tokens[1] -> (0,1), ...
 }
 
-
-func (b *Board) get(x, y int) int{
-    return b.tokens[x + 9*y]    
+func (b *Board) get(x, y int) int {
+	return b.tokens[x+9*y]
 }
 
-func (b *Board) hyouji(){
-    for i := 0; i < 9; i++ {
-				for j := 0; j < 9; j++ {
-					fmt.Printf(b.get(i, j))
-				}
-				fmt.Println("")
-    }
+func (b *Board) hyouji() {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			fmt.Printf(b.get(i, j))
+		}
+		fmt.Println("")
+	}
 }
 
 func (b *Board) put(x, y, value int) {
@@ -73,4 +71,3 @@ func main() {
 	}
 	b.input()
 }
-
